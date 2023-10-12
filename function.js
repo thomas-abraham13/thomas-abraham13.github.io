7ed4003af04ed9dd9b98c1996fde4df05b1a28b6
Thomas Abraham <thomas.abraham@fxdigital.uk>
2023-10-12 17:36:45 +0100

    edited format and function name

--- function.js
+++ function.js
@@ -1,5 +1,5 @@
 // When the user clicks on <div>, open the popup
-function myFunction() {
+function pop() {
     var popup = document.getElementById("myPopup");
     popup.classList.toggle("show");
 }
