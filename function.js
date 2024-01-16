f29536ac19e137503ef619e46893a89a7c0c13a0
Thomas Abraham <thomas.abraham@fxdigital.uk>
2024-01-16 11:09:29 +0000

    inital commit

--- function.js
+++ function.js
@@ -1,5 +1,20 @@
-// When the user clicks on <div>, open the popup
+// Function to use a pop-up on the Resume page
 function pop() {
     var popup = document.getElementById("myPopup");
     popup.classList.toggle("show");
+}
+
+// Function to use tabs on Resume page
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
 }
